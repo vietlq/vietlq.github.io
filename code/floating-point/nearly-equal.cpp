7b12b739250049aaf0dfaf44870430c7c549ad56
Viet Le <vietlq85@gmail.com>
2017-08-06 17:07:31 +0100

    renamed

--- code/floating-point/nearly-equal.cpp
+++ code/floating-point/nearly-equal.cpp
@@ -5,16 +5,16 @@
 #include <cmath>
 #include <limits>
 
-const float FLOAT_UPPER = 0.2000000f;
-const float FLOAT_MID2  = 0.1999999f;
-const float FLOAT_MID   = 0.1999990f;
-const float FLOAT_LOWER = 0.1999980f;
-const float FLOAT_EPS6  = 1e-6f;
+const float FLOAT_UPPER      = 0.2000000f;
+const float FLOAT_MID2       = 0.1999999f;
+const float FLOAT_MID        = 0.1999990f;
+const float FLOAT_LOWER      = 0.1999980f;
+const float FLOAT_MAX_DIFF6  = 1e-6f;
+const float FLOAT_MAX_DIFF7  = 1e-7f;
 // Note that 1e-7f is smaller than machine epsilon for floats.
-// So one cannot reliable use 1e-7f for epsilon between 2 floats.
-// One must use minimum epsilon = 2.**-23 = 1.1920928955078125e-07
-// when comparing 2 floats
-const float FLOAT_EPS7  = std::numeric_limits<float>::epsilon();
+// So one cannot naively use 1e-7f as difference between 2 floats.
+// Machine epsilon for float (32b) = 2.**-23 = 1.1920928955078125e-07
+//const float FLOAT_EPSILON  = std::numeric_limits<float>::epsilon();
 
 using Float32 = ::testing::internal::FloatingPoint<float>;
 using Float64 = ::testing::internal::FloatingPoint<double>;
@@ -46,15 +46,15 @@ TEST(NearlyEqual, Float6AbsUpperMid)
 {
     const float diff = std::abs(FLOAT_UPPER - FLOAT_MID);
     std::cout << "Float6AbsUpperMid: diff = " << diff << std::endl;
-    EXPECT_TRUE(diff < FLOAT_EPS6) << "diff = " << diff
-        << " is greater than FLOAT_EPS6 = " << FLOAT_EPS6;
+    EXPECT_TRUE(diff < FLOAT_MAX_DIFF6) << "diff = " << diff
+        << " is greater than FLOAT_MAX_DIFF6 = " << FLOAT_MAX_DIFF6;
 }
 
 TEST(NearlyEqual, Double6AbsUpperMid)
 {
     // Look at the source code and see that EXPECT_NEAR is using double
     // https://github.com/google/googletest/blob/078d5d930ad8b25b3c61556dc6de7ad13fa9a179/googletest/include/gtest/gtest.h#L2043
-    EXPECT_NEAR(FLOAT_UPPER, FLOAT_MID, FLOAT_EPS6);
+    EXPECT_NEAR(FLOAT_UPPER, FLOAT_MID, FLOAT_MAX_DIFF6);
 }
 
 TEST(NearlyEqual, Float6UlpUpperMid)
@@ -67,15 +67,15 @@ TEST(NearlyEqual, Float6AbsLowerMid)
 {
     const float diff = std::abs(FLOAT_LOWER - FLOAT_MID);
     std::cout << "Float6AbsLowerMid : diff = " << diff << std::endl;
-    EXPECT_TRUE(diff < FLOAT_EPS6) << "diff = " << diff
-        << " is greater than FLOAT_EPS6 = " << FLOAT_EPS6;
+    EXPECT_TRUE(diff < FLOAT_MAX_DIFF6) << "diff = " << diff
+        << " is greater than FLOAT_MAX_DIFF6 = " << FLOAT_MAX_DIFF6;
 }
 
 TEST(NearlyEqual, Double6AbsLowerMid)
 {
     // Look at the source code and see that EXPECT_NEAR is using double
     // https://github.com/google/googletest/blob/078d5d930ad8b25b3c61556dc6de7ad13fa9a179/googletest/include/gtest/gtest.h#L2043
-    EXPECT_NEAR(FLOAT_LOWER, FLOAT_MID, FLOAT_EPS6);
+    EXPECT_NEAR(FLOAT_LOWER, FLOAT_MID, FLOAT_MAX_DIFF6);
 }
 
 TEST(NearlyEqual, Float6UlpLowerMid)
@@ -88,15 +88,15 @@ TEST(NearlyEqual, Float7AbsUpperMid2)
 {
     const float diff = std::abs(FLOAT_UPPER - FLOAT_MID2);
     std::cout << "Float7AbsUpperMid2: diff = " << diff << std::endl;
-    EXPECT_TRUE(diff < FLOAT_EPS7) << "diff = " << diff
-        << " is greater than FLOAT_EPS7 = " << FLOAT_EPS7;
+    EXPECT_TRUE(diff < FLOAT_MAX_DIFF7) << "diff = " << diff
+        << " is greater than FLOAT_MAX_DIFF7 = " << FLOAT_MAX_DIFF7;
 }
 
 TEST(NearlyEqual, Double7AbsUpperMid2)
 {
     // Look at the source code and see that EXPECT_NEAR is using double
     // https://github.com/google/googletest/blob/078d5d930ad8b25b3c61556dc6de7ad13fa9a179/googletest/include/gtest/gtest.h#L2043
-    EXPECT_NEAR(FLOAT_UPPER, FLOAT_MID2, FLOAT_EPS7);
+    EXPECT_NEAR(FLOAT_UPPER, FLOAT_MID2, FLOAT_MAX_DIFF7);
 }
 
 TEST(NearlyEqual, Float7UlpUpperMid2)
