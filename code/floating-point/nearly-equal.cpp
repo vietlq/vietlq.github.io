b9102749083568ab6b8a7fc6999731ca03130747
Viet Le <vietlq85@gmail.com>
2017-08-06 11:55:05 +0100

    added double test cases

--- code/floating-point/nearly-equal.cpp
+++ code/floating-point/nearly-equal.cpp
@@ -37,6 +37,13 @@ TEST(NearlyEqual, Float6AbsLowerMid)
         << " is greater than FLOAT_EPS6 = " << FLOAT_EPS6;
 }
 
+TEST(NearlyEqual, Double6AbsLowerMid)
+{
+    // Look at the source code and see that EXPECT_NEAR is using double
+    // https://github.com/google/googletest/blob/078d5d930ad8b25b3c61556dc6de7ad13fa9a179/googletest/include/gtest/gtest.h#L2043
+    EXPECT_NEAR(FLOAT_LOWER, FLOAT_MID, FLOAT_EPS6);
+}
+
 TEST(NearlyEqual, Float6UlpLowerMid)
 {
     EXPECT_FLOAT_EQ(FLOAT_LOWER, FLOAT_MID);
@@ -48,6 +55,13 @@ TEST(NearlyEqual, Float7AbsUpperMid2)
     std::cout << "Float7AbsUpperMid2: diff = " << diff << std::endl;
     EXPECT_TRUE(diff < FLOAT_EPS7) << "diff = " << diff
         << " is greater than FLOAT_EPS7 = " << FLOAT_EPS7;
+}
+
+TEST(NearlyEqual, Double7AbsUpperMid2)
+{
+    // Look at the source code and see that EXPECT_NEAR is using double
+    // https://github.com/google/googletest/blob/078d5d930ad8b25b3c61556dc6de7ad13fa9a179/googletest/include/gtest/gtest.h#L2043
+    EXPECT_NEAR(FLOAT_UPPER, FLOAT_MID2, FLOAT_EPS7);
 }
 
 TEST(NearlyEqual, Float7UlpUpperMid2)
